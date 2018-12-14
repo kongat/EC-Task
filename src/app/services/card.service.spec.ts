259c73c7b6467e36e5d90868ddaa0bcf8db5268c
kongat <constantinos.gatis@gmail.com>
2018-12-14 22:49:27 +0200

    add import to service spec

--- src/app/services/card.service.spec.ts
+++ src/app/services/card.service.spec.ts
@@ -1,9 +1,11 @@
 import { TestBed } from '@angular/core/testing';
-
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { CardService } from './card.service';
 
 describe('CardService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  beforeEach(() => TestBed.configureTestingModule({
+    imports: [ HttpClientTestingModule]
+  }));
 
   it('should be created', () => {
     const service: CardService = TestBed.get(CardService);
